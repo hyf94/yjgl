68e2f2fc2f938ab5310683dadf6cafca4a2a1018
hyf <huangyf@sucsoft.com>
2019-08-26 10:31:49 +0800

    新增拨打电话

--- app/src/main/java/com/sucsoft/yjgl/core/webview/X5WebView.java
+++ app/src/main/java/com/sucsoft/yjgl/core/webview/X5WebView.java
@@ -1,13 +1,21 @@
 package com.sucsoft.yjgl.core.webview;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
+import com.sucsoft.yjgl.MainActivity;
+import com.sucsoft.yjgl.core.AppManager;
 import com.tencent.smtt.sdk.QbSdk;
 import com.tencent.smtt.sdk.WebSettings;
 import com.tencent.smtt.sdk.WebSettings.LayoutAlgorithm;
@@ -15,13 +23,35 @@ import com.tencent.smtt.sdk.WebView;
 import com.tencent.smtt.sdk.WebViewClient;
 
 public class X5WebView extends WebView {
+	private Context context;
 
 	private WebViewClient client = new WebViewClient() {
 		/**
 		 * 防止加载网页时调起系统浏览器
 		 */
 		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			view.loadUrl(url);
+
+//			view.loadUrl(url);
+			Log.i("用户单击超连接", url);
+			//判断用户单击的是那个超连接
+			String tag = "tel";
+			if (url.contains(tag)) {
+				String mobile = url.substring(url.lastIndexOf("/") + 1);
+				Log.e("mobile----------->",mobile);
+				Intent mIntent = new Intent(Intent.ACTION_CALL);
+				Uri data = Uri.parse(mobile);
+				mIntent.setData(data);
+				//Android6.0以后的动态获取打电话权限
+				if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+					AppManager.getAppManager().currentActivity().startActivity(mIntent);
+					//这个超连接,java已经处理了，webview不要处理
+					return true;
+				}else{
+					//申请权限
+					ActivityCompat.requestPermissions(AppManager.getAppManager().currentActivity(), new String[]{Manifest.permission.CALL_PHONE},1);
+					return true;
+				}
+			}
 			return true;
 		}
 	};
@@ -29,6 +59,7 @@ public class X5WebView extends WebView {
 	@SuppressLint("SetJavaScriptEnabled")
 	public X5WebView(Context arg0, AttributeSet arg1) {
 		super(arg0, arg1);
+		this.context = arg0;
 		this.setWebViewClient(client);
 		// this.setWebChromeClient(chromeClient);
 		// WebStorage webStorage = WebStorage.getInstance();
